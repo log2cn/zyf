e4e5a877920082e98da201c3517c03821b0742b2
log2 <log2cn@gmail.com>
2024-10-26 00:10:54 +0800

    1

--- upload.sh
+++ upload.sh
@@ -10,8 +10,7 @@ git add .
 git config user.email "log2cn@gmail.com"
 git config user.name "log2"
 
-branch_name=$(cat /dev/urandom | xxd -p | head -c 16)
-git checkout -b "$branch_name"
+git checkout -b $(xxd -p -l 8 /dev/urandom)
 
 git commit -m "upload files"
 git push -u -q origin "$branch_name" 2>&1 | grep -v '^remote:'
