1bf6ee4ede32e6c53750fbe56ebf9ad980503337
log2 <log2cn@gmail.com>
2024-10-26 14:14:35 +0800

    update

--- upload.sh
+++ upload.sh
@@ -4,11 +4,13 @@ trap "cd .. && rm -rf tmp" EXIT
 mv ../upload/* .
 
 git init --initial-branch=main 
-REPO_URL="https://log2:$PASSWORD@git.nju.edu.cn/log2/zyf_data.git"
-git remote add origin "$REPO_URL"
-git add .
+git remote add origin "https://log2:$PASSWORD@git.nju.edu.cn/log2/zyf_data.git"
+
 git config user.email "log2cn@gmail.com"
 git config user.name "log2"
-git checkout -b $(xxd -p -l 8 /dev/urandom)
-git commit -m "upload files"
-git push -u -q origin "$branch_name" 2>&1 | grep -v '^remote:'
+
+branch_name=$(xxd -p -l 8 /dev/urandom)
+git checkout -b $branch_name
+git add .
+git commit -m "upload files from github" > /dev/null
+git push -u origin $branch_name
