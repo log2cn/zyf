8b337173659b2498c27b93c388250e8f8a6864c1
log2 <log2cn@gmail.com>
2024-10-27 18:04:47 +0800

    update

--- get_data_list.sh
+++ get_data_list.sh
@@ -1,4 +1,4 @@
-git clone -q -b list https://log2:$TOKEN@git.nju.edu.cn/log2/zyf_data.git tmp && cd tmp
+git clone -q -b list https://$TOKEN@git.nju.edu.cn/log2/zyf_data.git tmp && cd tmp
 git push -q origin --delete list
 mv -f ./* ..
 cd ..
