d983215fea7de1b4884a6576ede84b6b5c552849
log2 <log2cn@gmail.com>
2024-10-27 18:21:05 +0800

    update

--- get_data_list.sh
+++ get_data_list.sh
@@ -1,5 +1,12 @@
+REPO_URL="https://$TOKEN@github.com/l0g2/zyf_imgs.git"
+
+if ! git ls-remote --heads "$REPO_URL" list | grep -q list ; then
+    exit
+fi
+
 git clone -q -b list https://$TOKEN@github.com/l0g2/zyf_imgs.git tmp && cd tmp
 git push -q origin --delete list
 mv ./* ..
 cd ..
 rm -rf tmp
+ls
