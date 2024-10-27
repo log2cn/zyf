7ae21ce948f0b7b25ddc8704c0d24ea4be409149
log2 <log2cn@gmail.com>
2024-10-28 00:00:14 +0800

    update

--- upload_nju.sh
+++ upload_nju.sh
@@ -1,15 +1,10 @@
-REPO="https://log2:$PASSWORD@git.nju.edu.cn/log2/zyf_data.git"
-
-git clone -q $REPO tmp && cd tmp
+git clone -q https://log2:$PASSWORD@git.nju.edu.cn/log2/zyf_data.git tmp && cd tmp
 trap "cd .. && rm -rf tmp" EXIT
 
 git config user.email "log2cn@gmail.com"
 git config user.name "log2"
 
-git push -q origin --delete a
-git checkout -b a
-
-touch a.txt
+echo $(xxd -p -l 8 /dev/urandom) > rand.txt
 git add .
 git commit -m "upload from nju" > /dev/null
-git push -q origin list 2>&1 | grep -v "^remote:"
+git push -q origin main 2>&1 | grep -v "^remote:"
