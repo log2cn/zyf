9e49137b55c0c9a4d28e931b1e03d4a3b88a9eb6
log2 <log2cn@gmail.com>
2024-10-28 11:35:59 +0800

    update

---
+++ main.sh
@@ -0,0 +1,14 @@
+echo $(cat data.json) | jq -c '.[]' | while read -r img; do
+  local_path=$(echo "$img" | jq -r '.[0]')
+  img_url=$(echo "$img" | jq -r '.[1]')
+
+  mkdir -p data/$(dirname $local_path)
+
+  for ((i=0; i<5; i++)); do
+    curl -s -o $local_path $img_url
+    
+    if [[ $? -eq 0 ]]; then
+      break
+    fi
+  done
+done
