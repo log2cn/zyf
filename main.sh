fb0496158d214547f80f73a2fdd21d194fd2ccd8
log2 <log2cn@gmail.com>
2024-10-28 11:48:58 +0800

    update

--- main.sh
+++ main.sh
@@ -1,3 +1,5 @@
+rclone move --ignore-errors box:/data.json .
+
 echo $(cat data.json) | jq -c '.[]' | while read -r img; do
   path=data/$(echo "$img" | jq -r '.[0]')
   url=$(echo "$img" | jq -r '.[1]')
@@ -7,8 +9,11 @@ echo $(cat data.json) | jq -c '.[]' | while read -r img; do
   for ((i=0; i<5; i++)); do
     curl -s -o $path $url
     if [[ $? -eq 0 ]]; then
-      echo "$path i"
+      echo "$path $i"
       break
     fi
   done
 done
+
+ls data
+rclone move --delete-empty-src-dirs data/ box:data/
