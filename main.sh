6ba1deb4eda0f5188806f6372b4455879f3d2194
log2 <log2cn@gmail.com>
2024-10-31 15:03:54 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 rclone cat box:data.txt | while read -r path url; do
   mkdir -p $(dirname $path)
-  curl -sS --retry 5 -o $path $url
+  curl -sS --retry 5 -o data/$path $url
 done
 
 time rclone move --delete-empty-src-dirs data/ box:data/
