12502191c84eb31ffc1c25cb901eb18f8cea4ffc
log2 <log2cn@gmail.com>
2024-11-13 14:12:38 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -5,3 +5,6 @@ rclone cat box:data.txt | while read -r path url; do
 done
 
 time rclone move --delete-empty-src-dirs data/ box:$(date +"%Y%m%d_%H%M")/
+
+curl -X POST --header "PRIVATE-TOKEN: $GITLAB_TOKEN" \
+    "https://git.nju.edu.cn/api/v4/projects/log2%2fzyf_nas/pipeline?ref=main"
