df09e0945eb9588af76c98edeef9a71cf9a3b3fc
log2 <log2cn@gmail.com>
2024-11-13 19:45:36 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -4,13 +4,13 @@ GITLAB_REPO="https://git.nju.edu.cn/api/v4/projects/log2%2fzyf_nas"
 GITLAB_HEADER="PRIVATE-TOKEN: $GITLAB_TOKEN"
 
 curl -sSf -H "$GITLAB_HEADER" "$GITLAB_REPO/repository/files/nmc_targets.txt/raw" \
-  | python3 main.py \
-  | while read -r url path; do
-      path="$DATA_DIR/$path"
-      mkdir -p $(dirname $path)
-      curl -sS --retry 5 -o $path $url
-      # break # for test
-    done
+| python3 main.py \
+| while read -r url path; do
+    path="$DATA_DIR/$path"
+    mkdir -p $(dirname $path)
+    curl -sS --retry 5 -o $path $url
+    # break # for test
+  done
 # exit # for test
 
 # data -> box
