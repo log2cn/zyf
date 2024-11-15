f6b4d1479a9ee2e33d7d0a325a1595ae3df361b8
log2 <log2cn@gmail.com>
2024-11-15 16:07:57 +0800

    update

--- main.sh
+++ main.sh
@@ -1,10 +1,18 @@
-PYTHONDONTWRITEBYTECODE=1 # disable __pycache__ 
-GITLAB_REPO="https://git.nju.edu.cn/api/v4/projects/log2%2fzyf_nas"
-GITLAB_HEADER="PRIVATE-TOKEN: $GITLAB_TOKEN"
+REPO="https://git.nju.edu.cn/api/v4/projects/13817"
+UPLOAD_URL=$(
+  curl -sSf -H "Authorization: Token $BOX_TOKEN" \
+  https://box.nju.edu.cn/api2/repos/cdd3071b-dce0-486d-8e6d-497a921a340d/upload-link/ \
+  | tr -d "\""
+)
 
-curl -sSf -H "$GITLAB_HEADER" "$GITLAB_REPO/repository/files/nmc_targets.txt/raw" \
+curl -sSf -H "PRIVATE-TOKEN: $GITLAB_TOKEN" "$REPO/repository/files/nmc_targets.txt/raw" \
 | python3 main.py \
-| rclone rcat zyf:targets.txt
+> targets.txt
+
+curl -H "Authorization: Token $BOX_TOKEN" $UPLOAD_URL \
+    -F file=@targets.txt -F parent_dir=/ -F replace=1
+
+rm targets.txt
 
 # trigger next steps
-curl -sSf -o /dev/null -X POST -H "$GITLAB_HEADER" "$GITLAB_REPO/pipeline?ref=main"
+curl -sSf -H "PRIVATE-TOKEN: $GITLAB_TOKEN" "$REPO/pipeline?ref=main" -X POST > /dev/null
