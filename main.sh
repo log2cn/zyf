f0a127f14c7f819e6178c3c47d5466149b918e1b
log2 <log2cn@gmail.com>
2025-02-11 15:22:33 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,15 +1,16 @@
 GIT_REPO="https://git.nju.edu.cn/api/v4/projects/13817"
 GIT_HEAD="PRIVATE-TOKEN: $GITLAB_TOKEN"
+
 BOX_REPO="https://box.nju.edu.cn/api2/repos/cdd3071b-dce0-486d-8e6d-497a921a340d"
 BOX_HEAD="Authorization: Token $BOX_TOKEN"
+BOX_UPLOAD_URL=$( curl -sSf -H "$BOX_HEAD" "$BOX_REPO/upload-link/" | tr -d "\"" )
 
 curl -sSf -H "$GIT_HEAD" "$GIT_REPO/repository/files/nmc_targets.txt/raw" \
 | python3 main.py \
 > targets.txt
 
 # upload targets.txt to box
-box_upload_url=$( curl -sSf -H "$BOX_HEAD" "$BOX_REPO/upload-link/" | tr -d "\"" )
-curl -sSf -F file=@targets.txt -F parent_dir=/ -F replace=1 "$box_upload_url"
+curl -sSf -F file=@targets.txt -F parent_dir=/ -F replace=1 "$BOX_UPLOAD_URL"
 rm targets.txt
 
 # trigger next steps
