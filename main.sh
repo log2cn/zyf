546c97efb31528e4e7e72000a94d32ae41493b1b
log2 <log2cn@gmail.com>
2025-02-11 15:19:27 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,16 +1,16 @@
-REPO="https://git.nju.edu.cn/api/v4/projects/13817"
+GIT_REPO="https://git.nju.edu.cn/api/v4/projects/13817"
+GIT_HEAD="PRIVATE-TOKEN: $GITLAB_TOKEN"
+BOX_REPO="https://box.nju.edu.cn/api2/repos/cdd3071b-dce0-486d-8e6d-497a921a340d"
+BOX_HEAD="Authorization: Token $BOX_TOKEN"
 
-curl -sSf -H "PRIVATE-TOKEN: $GITLAB_TOKEN" "$REPO/repository/files/nmc_targets.txt/raw" \
+curl -sSf -H "$GIT_HEAD" "$GIT_REPO/repository/files/nmc_targets.txt/raw" \
 | python3 main.py \
 > targets.txt
 
-curl -sSf -F file=@targets.txt -F parent_dir=/ -F replace=1 $(
-  curl -sSf -H "Authorization: Token $BOX_TOKEN" \
-  https://box.nju.edu.cn/api2/repos/cdd3071b-dce0-486d-8e6d-497a921a340d/upload-link/ \
-  | tr -d "\""
-)
-
+# upload targets.txt to box
+box_upload_url = $( curl -sSf -H "$BOX_HEAD" "$BOX_REPO/upload-link/" | tr -d "\"" )
+curl -sSf -F file=@targets.txt -F parent_dir=/ -F replace=1 "$box_upload_url"
 rm targets.txt
 
 # trigger next steps
-curl -sSf -H "PRIVATE-TOKEN: $GITLAB_TOKEN" "$REPO/pipeline?ref=main" -X POST > /dev/null
+curl -sSf -H "$GIT_HEAD" "$GIT_REPO/pipeline?ref=main" -X POST > /dev/null
