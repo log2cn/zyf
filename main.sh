cb3ce77bd605f2df9cce6752dc626beca3494ad4
log2 <log2cn@gmail.com>
2025-03-03 12:45:54 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -5,13 +5,13 @@ BOX_REPO="https://box.nju.edu.cn/api2/repos/cdd3071b-dce0-486d-8e6d-497a921a340d
 BOX_HEAD="Authorization: Token $BOX_TOKEN"
 BOX_UPLOAD_URL=$( curl -sSf -H "$BOX_HEAD" "$BOX_REPO/upload-link/" | tr -d "\"" )
 
-curl -sSf -H "$GIT_HEAD" "$GIT_REPO/repository/files/nmc_targets.txt/raw" \
+curl -sSf -H "$GIT_HEAD" "$GIT_REPO/repository/files/html_targets.txt/raw" \
 | python3 main.py \
-> targets.txt
+> png_targets.txt
 
-# upload targets.txt to box
-curl -sSf -F file=@targets.txt -F parent_dir=/ -F replace=1 "$BOX_UPLOAD_URL"
-rm targets.txt
+# upload to box
+curl -sSf -F file=@png_targets.txt -F parent_dir=/ -F replace=1 "$BOX_UPLOAD_URL"
+rm png_targets.txt
 
 # trigger next steps
 curl -sSf -H "$GIT_HEAD" "$GIT_REPO/pipeline?ref=main" -X POST
