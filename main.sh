8e4e7235f34cc212b1d1d5c2de4379a6542b75de
log2 <log2cn@gmail.com>
2025-03-04 19:43:15 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -15,7 +15,7 @@ curl -sSf -H "$GIT_HEAD" "$GIT_REPO/repository/files/html_targets.txt/raw" \
 | python3 main.py \
 | tr '\n' ' ' \
 | curl -sSf -X PUT -H "$GIT_HEAD" "$GIT_REPO/variables/PNG_TARGETS" --form "value=$(cat)" \
-| cut -c 1-80
+| wc -c
 
 # trigger next steps
 curl -sSf -X POST -H "$GIT_HEAD" "$GIT_REPO/pipeline?ref=main" 
