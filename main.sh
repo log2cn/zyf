e50ebf0bb664d9321c471e239dbe08f4b536371d
log2 <log2cn@gmail.com>
2025-03-04 19:26:57 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -6,10 +6,15 @@ BOX_HEAD="Authorization: Token $BOX_TOKEN"
 BOX_UPLOAD_URL=$( curl -sSf -H "$BOX_HEAD" "$BOX_REPO/upload-link/" | tr -d "\"" )
 
 # html_targets -> png_targets
+# curl -sSf -H "$GIT_HEAD" "$GIT_REPO/repository/files/html_targets.txt/raw" \
+# | python3 main.py \
+# | curl -sSf -F file="@-;filename=png_targets.txt" -F parent_dir=/ -F replace=1 "$BOX_UPLOAD_URL"
+
+# html_targets -> png_targets
 curl -sSf -H "$GIT_HEAD" "$GIT_REPO/repository/files/html_targets.txt/raw" \
 | python3 main.py \
-| curl -sSf -F file="@-;filename=png_targets.txt" -F parent_dir=/ -F replace=1 "$BOX_UPLOAD_URL"
+| tr '\n' ' ' \
+| curl -sSf -X PUT -H "$GIT_HEAD" "$GIT_REPO/variables/PNG_TARGETS" --form "value=$(cat)" 
 
 # trigger next steps
-curl -sSf -X PUT  -H "$GIT_HEAD" "$GIT_REPO/variables/PNG_TARGETS" --form "value=1 2 4 5" 
 curl -sSf -X POST -H "$GIT_HEAD" "$GIT_REPO/pipeline?ref=main" 
