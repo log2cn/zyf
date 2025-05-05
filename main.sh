a976dfcffa982a2d8d3886e09ceb7b9d56de42d2
log2 <log2cn@gmail.com>
2025-05-05 17:12:11 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,7 +1,6 @@
 REPO="https://git.nju.edu.cn/api/v4/projects/13817"
 CURL="curl -sSf -H PRIVATE-TOKEN:$GITLAB_TOKEN --retry 3"
 
-set -x
 $CURL "$REPO/repository/files/html_targets.txt/raw" \
 | python3 main.py \
 | tr '\n' ' ' \
