8a5445f8f583f1dd08701422fd43af7372504c23
log2 <log2cn@gmail.com>
2025-05-05 17:01:04 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -4,7 +4,7 @@ CURL="curl -sSf -H PRIVATE-TOKEN:$GITLAB_TOKEN --retry 3"
 $CURL "$REPO/repository/files/html_targets.txt/raw" \
 | python3 main.py \
 | tr '\n' ' ' \
-| $CURL "$REPO/variables/PNG_TARGETS" -X PUT -F "value=@-" \
+| $CURL "$REPO/variables/PNG_TARGETS" -X PUT -F "value=@*" \
 | wc -c
 
 # $CURL "$REPO/repository/files/html_targets.txt/raw" \
