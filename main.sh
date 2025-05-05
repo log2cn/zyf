67e4a0f23d8cc5253727e6c16045aeae07ed89db
log2 <log2cn@gmail.com>
2025-05-05 17:06:26 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,10 +1,11 @@
 REPO="https://git.nju.edu.cn/api/v4/projects/13817"
 CURL="curl -sSf -H PRIVATE-TOKEN:$GITLAB_TOKEN --retry 3"
 
+set -x
 $CURL "$REPO/repository/files/html_targets.txt/raw" \
 | python3 main.py \
 | tr '\n' ' ' \
-| $CURL "$REPO/variables/PNG_TARGETS" -X PUT -F "value=@*" \
+| $CURL "$REPO/variables/PNG_TARGETS" -X PUT -F "value=@-" \
 | wc -c
 
 # $CURL "$REPO/repository/files/html_targets.txt/raw" \
