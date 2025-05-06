cc66d9c0c64d8dca7b15f37811871b8b449868a2
log2 <log2cn@gmail.com>
2025-05-06 20:01:46 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -4,9 +4,9 @@ CURL="curl -sSf -H PRIVATE-TOKEN:$GITLAB_TOKEN --retry 3"
 $CURL "$REPO/repository/files/html_targets.txt/raw" \
 | python3 main.py \
 | tee >(wc -l) \
-  >(sed -n   '1,200p' | $CURL "$REPO/variables/PNG_TARGETS0" -X PUT -F value=@- | wc -c) \
-  >(sed -n '201,400p' | $CURL "$REPO/variables/PNG_TARGETS1" -X PUT -F value=@- | wc -c) \
-  >(sed -n '401,600p' | $CURL "$REPO/variables/PNG_TARGETS1" -X PUT -F value=@- | wc -c) \
+  >(sed -n   '1,200p' | tr '\n' ' ' | $CURL "$REPO/variables/PNG_TARGETS0" -X PUT -F value=@- | wc -c) \
+  >(sed -n '201,400p' | tr '\n' ' ' | $CURL "$REPO/variables/PNG_TARGETS1" -X PUT -F value=@- | wc -c) \
+  >(sed -n '401,600p' | tr '\n' ' ' | $CURL "$REPO/variables/PNG_TARGETS1" -X PUT -F value=@- | wc -c) \
 > /dev/null
 
 $CURL "$REPO/pipeline?ref=main" -X POST | wc -c
