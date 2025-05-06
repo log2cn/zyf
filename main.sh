16f29863865653f6604bf186604b2b9b1c0b75c5
log2 <log2cn@gmail.com>
2025-05-06 20:14:22 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -3,10 +3,9 @@ CURL="curl -sSf -H PRIVATE-TOKEN:$GITLAB_TOKEN --retry 3"
 
 $CURL $REPO/repository/files/html_targets.txt/raw \
 | python3 main.py \
-| tee >(wc -l) \
-  >(sed -n     1,500p | tr '\n' ' ' | xargs -I {} $CURL $REPO/variables/PNG_TARGETS0 -X PUT -F value={} | wc -c) \
-  >(sed -n  501,1000p | tr '\n' ' ' | xargs -I {} $CURL $REPO/variables/PNG_TARGETS1 -X PUT -F value={} | wc -c) \
-  >(sed -n 1001,1500p | tr '\n' ' ' | xargs -I {} $CURL $REPO/variables/PNG_TARGETS2 -X PUT -F value={} | wc -c) \
-> /dev/null
+| tee >(sed -n     1,500p | tr '\n' ' ' | xargs -I {} $CURL $REPO/variables/PNG_TARGETS0 -X PUT -F value={} | wc -c) \
+| tee >(sed -n  501,1000p | tr '\n' ' ' | xargs -I {} $CURL $REPO/variables/PNG_TARGETS1 -X PUT -F value={} | wc -c) \
+| tee >(sed -n 1001,1500p | tr '\n' ' ' | xargs -I {} $CURL $REPO/variables/PNG_TARGETS2 -X PUT -F value={} | wc -c) \
+| wc -l
 
 $CURL $REPO/pipeline?ref=main -X POST | wc -c
