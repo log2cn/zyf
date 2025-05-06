969ce159baffd631d6ad63d71249f84b62077e09
log2 <log2cn@gmail.com>
2025-05-06 20:39:52 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,11 +1,10 @@
 REPO="https://git.nju.edu.cn/api/v4/projects/13817"
 CURL="curl -sSf -H PRIVATE-TOKEN:$GITLAB_TOKEN --retry 3"
 
-$CURL $REPO/repository/files/html_targets.txt/raw | python3 main.py | {
-  sed -n     1,500p | tr '\n' ' ' | xargs -I {} $CURL $REPO/variables/PNG_TARGETS0 -X PUT -F value={} | cut -c 1-80
-  sed -n  501,1000p | tr '\n' ' ' | xargs -I {} $CURL $REPO/variables/PNG_TARGETS1 -X PUT -F value={} | cut -c 1-80
-  sed -n 1001,1500p | tr '\n' ' ' | xargs -I {} $CURL $REPO/variables/PNG_TARGETS2 -X PUT -F value={} | cut -c 1-80
-  wc -l
-}
+$CURL $REPO/repository/files/html_targets.txt/raw | python3 main.py > png_targets.txt
+
+sed -n     1,500p png_targets.txt | tr '\n' ' ' | xargs -I {} $CURL $REPO/variables/PNG_TARGETS0 -X PUT -F value={} | wc -c
+sed -n  501,1000p png_targets.txt | tr '\n' ' ' | xargs -I {} $CURL $REPO/variables/PNG_TARGETS1 -X PUT -F value={} | wc -c
+sed -n 1001,1500p png_targets.txt | tr '\n' ' ' | xargs -I {} $CURL $REPO/variables/PNG_TARGETS2 -X PUT -F value={} | wc -c
 
 $CURL $REPO/pipeline?ref=main -X POST | wc -c
