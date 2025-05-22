369cf228d0ce3d8230f90e41bb6664fe447aac8a
log2 <log2cn@gmail.com>
2025-05-22 14:42:13 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -9,8 +9,8 @@ $CURL $REPO/variables/PNG_TARGETS0 -X DELETE
 $CURL $REPO/variables/PNG_TARGETS1 -X DELETE
 $CURL $REPO/variables/PNG_TARGETS2 -X DELETE
 
-sed -n     1,500p png_targets.txt | tr '\n' ' ' | xargs -I {} $CURL $REPO/variables -X POST -F key=PNG_TARGETS0 -F value={} | wc -c
-sed -n  501,1000p png_targets.txt | tr '\n' ' ' | xargs -I {} $CURL $REPO/variables -X POST -F key=PNG_TARGETS1 -F value={} | wc -c
-sed -n 1001,1500p png_targets.txt | tr '\n' ' ' | xargs -I {} $CURL $REPO/variables -X POST -F key=PNG_TARGETS2 -F value={} | wc -c
+sed -n     1,500p png_targets.txt | tr '\n' ' ' | $CURL $REPO/variables -X POST -F key=PNG_TARGETS0 -F "value=$(cat)" | wc -c
+sed -n  501,1000p png_targets.txt | tr '\n' ' ' | $CURL $REPO/variables -X POST -F key=PNG_TARGETS1 -F "value=$(cat)" | wc -c
+sed -n 1001,1500p png_targets.txt | tr '\n' ' ' | $CURL $REPO/variables -X POST -F key=PNG_TARGETS2 -F "value=$(cat)" | wc -c
 
 $CURL $REPO/pipeline?ref=main -X POST | wc -c
