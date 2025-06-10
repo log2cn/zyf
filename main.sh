2d86fba5ef78da0e8b211dfcc281fdeae4a477df
log2 <log2cn@gmail.com>
2025-06-10 13:12:55 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 REPO="https://git.nju.edu.cn/api/v4/projects/13817"
-CURL="curl -sSf -H PRIVATE-TOKEN:$GITLAB_TOKEN --retry 3"
+CURL="curl -sSf --retry 3 -H PRIVATE-TOKEN:$GITLAB_TOKEN"
 
 echo "$(date "+%H:%M:%S"): get html_targets.txt, run main.py, save to png_targets.txt"
 $CURL $REPO/repository/files/html_targets.txt/raw | python3 main.py > png_targets.txt
